0075e06658d85fe7f2d1ff9c7facf36044a67ef1
Jill <1105105104@nkust.edu.tw>
2024-01-24 10:07:17 +0800

    加入專案檔案。

---
+++ Controllers/HomeworkController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxHomework.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HW01()
+        {
+            return View();
+        }
+
+        public IActionResult HW02()
+        {
+            return View();
+        }
+    }
+}
