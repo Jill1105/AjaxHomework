3428afac8d98456302fe087b90b679b39332c7fc
Jill <1105105104@nkust.edu.tw>
2024-01-24 17:25:42 +0800

    新增APIController

---
+++ Controllers/APIController.cs
@@ -0,0 +1,27 @@
+﻿using AjaxHomework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxHomework.Controllers
+{
+
+    public class APIController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+
+        public APIController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CheckAccount(string name)
+        {
+            Member member = _dbContext.Members.FirstOrDefault(a => a.Name == name);
+            if (member == null) return Json(false);
+            return Json(true);
+        }
+    }
+}
--- Controllers/HomeworkController.cs
+++ Controllers/HomeworkController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AjaxHomework.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 
 namespace AjaxHomework.Controllers
 {
     public class HomeworkController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
@@ -15,6 +19,15 @@ namespace AjaxHomework.Controllers
         }
 
         public IActionResult HW02()
+        {
+            return View();
+        }
+
+        public IActionResult HW03()
+        {
+            return View();
+        }
+        public IActionResult HW04()
         {
             return View();
         }
